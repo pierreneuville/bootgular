741b9ca27167f68025c2841d5b576453bedd2cd5
pierreneuville <pierre.neuville@gmail.com>
2019-12-17 04:10:38 +0100

    pi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { navbarcomponent } from './navbar/navbar.component';
+import { jumbotroncomponent } from './jumbotron/jumbotron.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    navbarcomponent,
+    jumbotroncomponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
